2fc5b1e07e83e1df36b51b2f5b66fc077823e546
mpatecki <marcinpatecki@gmail.com>
2024-08-21 23:17:02 +0200

    slightly modified version of the simulation code. Geometry improved, cuts added to the physics list in order to remove the spikes. Seems to be working

---
+++ BC2/src/PrimaryGeneratorAction.cc
@@ -0,0 +1,27 @@
+#include "PrimaryGeneratorAction.hh"
+#include "G4ParticleGun.hh"
+#include "G4ParticleTable.hh"
+#include "G4Proton.hh"
+#include "G4SystemOfUnits.hh"
+
+PrimaryGeneratorAction::PrimaryGeneratorAction() {
+    G4int n_particle = 1;
+    fParticleGun = new G4ParticleGun(n_particle);
+
+    G4ParticleTable* particleTable = G4ParticleTable::GetParticleTable();
+    G4ParticleDefinition* proton = particleTable->FindParticle("proton");
+
+    fParticleGun->SetParticleDefinition(proton);
+    fParticleGun->SetParticleEnergy(200*MeV);
+    fParticleGun->SetParticleMomentumDirection(G4ThreeVector(0., 0., 1.));
+    fParticleGun->SetParticlePosition(G4ThreeVector(0., 0., -0.15*m));
+}
+
+PrimaryGeneratorAction::~PrimaryGeneratorAction() {
+    delete fParticleGun;
+}
+
+void PrimaryGeneratorAction::GeneratePrimaries(G4Event* anEvent) {
+    fParticleGun->GeneratePrimaryVertex(anEvent);
+}
+
