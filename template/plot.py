b241f22378cc16f6591fc8d9983e287af93fa2c7
mpatecki <marcinpatecki@gmail.com>
2024-08-21 15:44:39 +0200

    add plot.py

---
+++ template/plot.py
@@ -0,0 +1,10 @@
+import matplotlib.pyplot as plt
+import pandas as pd
+import matplotlib.pyplot as plt
+
+
+fig, axs = plt.subplots(figsize=(12,4))
+df = pd.read_csv('BraggCurveData.txt', sep='\t', header='infer')
+df.plot(kind='line', x=df.columns[0], y=df.columns[1],ax=axs)
+
+plt.show()
